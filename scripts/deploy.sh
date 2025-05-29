8506662094c2234bee7a43fa54329fb7c5449e74
shaligatv <shaliga245@gmail.com>
2025-05-29 13:54:47 +0530

    Delete scripts directory

--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-kubectl apply -f kubernetes/configmap.yaml
-kubectl apply -f kubernetes/product/
-kubectl apply -f kubernetes/user/
-kubectl apply -f kubernetes/ingress.yaml
--- scripts/setup-monitoring.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
-helm install prometheus prometheus-community/prometheus -f monitoring/prometheus-values.yaml
-helm install grafana grafana/grafana --set adminPassword=admin
